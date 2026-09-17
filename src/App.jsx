cb9411b5c334c3a3adad7fe9e079cd96b0b6c140
cmadu9737-dev <cmadu9737@gmail.com>
2026-09-17 14:20:57 +0100

    added beauty

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import car from "./assets/car.jpg";
+import beauty from "./assets/beauty.jpg";
+import buyer from "./assets/buyer.png";
 
 const App = () => {
   return (
@@ -49,17 +51,17 @@ const App = () => {
         <h2>Feedback From Our Student</h2>
         <div className="testimonial-container">
             <div className="card">
-                <img src="../sample/beauty.jpg" alt="studend Testimonies"/>
+                <img src={beauty} alt="studend Testimonies"/>
                 <h3>Oluchi Iwueze</h3>
                 <p>This Academy completely changed my career. i learnt how to design from this program</p>
             </div>
             <div className="card">
-                <img src="../sample/buyer.png" alt="studend Testimonies"/>
+                <img src={buyer} alt="studend Testimonies"/>
                 <h3>Oluchi Iwueze</h3>
                 <p>head the spin me and am confused at first but later it started becoming so clearer thanks to the the program</p>
             </div>
             <div className="card">
-                <img   src="../sample/car.jpg" alt="studend Testimonies"/>
+                <img   src={car} alt="studend Testimonies"/>
                 <h3>Oluchi Iwueze</h3>
                 <p>This program helped me to become confident in my design</p>
             </div>
